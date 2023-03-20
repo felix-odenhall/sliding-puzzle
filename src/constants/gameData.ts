12766958c949d372f16db5b1e46d896d1660c167
Felix Odenhall <felix.odenhall@gmail.com>
2023-03-20 21:14:47 +0100

    updated gameData

--- src/constants/gameData.ts
+++ src/constants/gameData.ts
@@ -1,4 +1,6 @@
-export const TILE_AMOUNT = 15;
-export const GRID_COLS = 5;
-export const GRID_ROWS = 3;
-export const TILE_SIZE = 80;
+export const GRID_COLS: number = 5;
+export const GRID_ROWS: number = 3;
+
+export const TILE_AMOUNT: number = GRID_COLS * GRID_ROWS;
+
+export const TILE_SIZE: number = 80;
