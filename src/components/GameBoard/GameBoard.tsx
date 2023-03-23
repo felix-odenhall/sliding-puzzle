d83812c3fe4c415513e6c811d02b00bcfe2742ee
Felix Odenhall <felix.odenhall@gmail.com>
2023-03-23 10:38:10 +0100

    created the game board

--- src/components/GameBoard/GameBoard.tsx
+++ src/components/GameBoard/GameBoard.tsx
@@ -1,3 +1,27 @@
+import { useState } from "react";
+import styled from "styled-components";
+import { board } from "../../constants/gameData";
+
+const SGameboard = styled.div`
+  display: grid;
+  grid-template-rows: repeat(3, 1fr);
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2px;
+`;
+
 export const GameBoard = () => {
-  return <div>GameBoard</div>;
+  const [puzzleBoard, setPuzzleBoard] = useState(board);
+  return (
+    <SGameboard>
+      {puzzleBoard.map((rows) => {
+        return (
+          <>
+            {rows.map((columns) => {
+              return <button>{columns}</button>;
+            })}
+          </>
+        );
+      })}
+    </SGameboard>
+  );
 };
