0f9a8b25bbaf1aa93f8c9c9142b267bd0f7b8a6c
Felix Odenhall <felix.odenhall@gmail.com>
2023-03-23 14:02:14 +0100

    updated gameBoard and import functions

--- src/components/GameBoard/GameBoard.tsx
+++ src/components/GameBoard/GameBoard.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import { board } from "../../constants/gameData";
+import { initialColumns, initialRows } from "../../constants/gameData";
+import { checkNeighbourTile } from "../../utils/checkNeighbourTile";
+import { createPuzzle } from "../../utils/createPuzzle";
 
 const SGameboard = styled.div`
   display: grid;
-  grid-template-rows: repeat(3, 1fr);
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(${initialRows}, 1fr);
+  grid-template-columns: repeat(${initialColumns}, 1fr);
   gap: 2px;
 `;
 
@@ -14,64 +16,12 @@ const SPuzzleTile = styled.button<{ value: number }>`
 `;
 
 export const GameBoard = () => {
-  const [puzzleBoard, setPuzzleBoard] = useState(board);
-
-  const canMove = (
-    topTile: number | null,
-    bottomTile: number | null,
-    leftTile: number | null,
-    rightTile: number | null,
-    rowIndex: number,
-    colIndex: number
-  ) => {
-    if (topTile === 0) {
-      puzzleBoard[rowIndex - 1][colIndex] = puzzleBoard[rowIndex][colIndex];
-      puzzleBoard[rowIndex][colIndex] = 0;
-      setPuzzleBoard([...puzzleBoard]);
-    }
-    if (bottomTile === 0) {
-      puzzleBoard[rowIndex + 1][colIndex] = puzzleBoard[rowIndex][colIndex];
-      puzzleBoard[rowIndex][colIndex] = 0;
-      setPuzzleBoard([...puzzleBoard]);
-    }
-    if (leftTile === 0) {
-      puzzleBoard[rowIndex][colIndex - 1] = puzzleBoard[rowIndex][colIndex];
-      puzzleBoard[rowIndex][colIndex] = 0;
-      setPuzzleBoard([...puzzleBoard]);
-    }
-    if (rightTile === 0) {
-      puzzleBoard[rowIndex][colIndex + 1] = puzzleBoard[rowIndex][colIndex];
-      puzzleBoard[rowIndex][colIndex] = 0;
-      setPuzzleBoard([...puzzleBoard]);
-    }
-  };
-
-  const checkNeighbourTile = (rowIndex: number, colIndex: number) => {
-    const checkTopTile =
-      rowIndex > 0 ? puzzleBoard[rowIndex - 1][colIndex] : null;
-    const checkBottomTile =
-      rowIndex < puzzleBoard.length - 1
-        ? puzzleBoard[rowIndex + 1][colIndex]
-        : null;
-    const checkLeftTile =
-      colIndex > 0 ? puzzleBoard[rowIndex][colIndex - 1] : null;
-    const checkRightTile =
-      colIndex < puzzleBoard.length - 1
-        ? puzzleBoard[rowIndex][colIndex + 1]
-        : null;
-
-    canMove(
-      checkTopTile,
-      checkBottomTile,
-      checkLeftTile,
-      checkRightTile,
-      rowIndex,
-      colIndex
-    );
-  };
+  const [puzzleBoard, setPuzzleBoard] = useState(
+    createPuzzle(initialRows, initialColumns)
+  );
 
   const handleClickedTile = (rowIndex: number, colIndex: number) => {
-    checkNeighbourTile(rowIndex, colIndex);
+    checkNeighbourTile(rowIndex, colIndex, puzzleBoard, setPuzzleBoard);
   };
 
   return (
