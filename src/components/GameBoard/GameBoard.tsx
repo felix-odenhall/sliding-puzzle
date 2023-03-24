da4b801c5946cdd28b7a916fe165ae96de73cec0
Felix Odenhall <felix.odenhall@gmail.com>
2023-03-24 11:04:47 +0100

    updated imports and design

--- src/components/GameBoard/GameBoard.tsx
+++ src/components/GameBoard/GameBoard.tsx
@@ -1,18 +1,43 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import { initialColumns, initialRows } from "../../constants/gameData";
-import { checkNeighbourTile } from "../../utils/checkNeighbourTile";
-import { createPuzzle } from "../../utils/createPuzzle";
+import {
+  initialColumns,
+  initialRows,
+  tile_size,
+} from "../../constants/gameData";
+
+import {
+  checkNeighbourTile,
+  createPuzzle,
+  puzzleSolution,
+} from "../../utils/index";
+
+const SContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 80vh;
+`;
 
 const SGameboard = styled.div`
   display: grid;
   grid-template-rows: repeat(${initialRows}, 1fr);
   grid-template-columns: repeat(${initialColumns}, 1fr);
-  gap: 2px;
+  width: calc(${initialColumns} * ${tile_size});
+  max-width: 100%;
+  gap: 1px;
 `;
 
 const SPuzzleTile = styled.button<{ value: number }>`
   background-color: ${({ value }) => (value !== 0 ? "red" : "grey")};
+  width: ${tile_size};
+  height: ${tile_size};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.5rem;
+  font-weight: 700;
 `;
 
 export const GameBoard = () => {
@@ -25,26 +50,28 @@ export const GameBoard = () => {
   };
 
   return (
-    <SGameboard>
-      {puzzleBoard.map((rows, rowIndex) => {
-        return (
-          <React.Fragment key={rowIndex}>
-            {rows.map((value, colIndex) => {
-              return (
-                <SPuzzleTile
-                  key={value}
-                  value={value}
-                  onClick={() => {
-                    handleClickedTile(rowIndex, colIndex);
-                  }}
-                >
-                  {value !== 0 && value}
-                </SPuzzleTile>
-              );
-            })}
-          </React.Fragment>
-        );
-      })}
-    </SGameboard>
+    <SContainer>
+      <SGameboard>
+        {puzzleBoard.map((rows, rowIndex) => {
+          return (
+            <React.Fragment key={rowIndex}>
+              {rows.map((value, colIndex) => {
+                return (
+                  <SPuzzleTile
+                    key={value}
+                    value={value}
+                    onClick={() => {
+                      handleClickedTile(rowIndex, colIndex);
+                    }}
+                  >
+                    {value !== 0 && value}
+                  </SPuzzleTile>
+                );
+              })}
+            </React.Fragment>
+          );
+        })}
+      </SGameboard>
+    </SContainer>
   );
 };
