c28baf6423c6c6e8a7e4b72ddededce756860c46
Felix Odenhall <felix.odenhall@gmail.com>
2023-03-25 23:47:41 +0100

    added conditional tile_size

--- src/constants/gameData.ts
+++ src/constants/gameData.ts
@@ -1,3 +1,4 @@
 export const initialRows = 3;
 export const initialColumns = 3;
-export const tile_size = "5rem";
+export const tile_size =
+  (initialRows * initialColumns) / 3 >= 5 ? "3.5rem" : "5rem";
