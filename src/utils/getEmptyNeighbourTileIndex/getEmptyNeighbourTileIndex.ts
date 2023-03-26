dd51ccf5e1c5a77bfd43d1bf1562867c0bfeedb8
Felix Odenhall <felix.odenhall@gmail.com>
2023-03-26 15:28:18 +0200

    updated variable names in getEmptyNeighbourTileIndex

--- src/utils/getEmptyNeighbourTileIndex/getEmptyNeighbourTileIndex.ts
+++ src/utils/getEmptyNeighbourTileIndex/getEmptyNeighbourTileIndex.ts
@@ -4,26 +4,26 @@ export const getEmptyNeighbourTileIndex = (
   board: number[][]
 ) => {
   // Check the number of a tile above, under, left or right of the clicked tile
-  const checkAboveTile = rowIndex > 0 ? board[rowIndex - 1][colIndex] : null;
-  const checkBelowTile =
+  const checkTileAbove = rowIndex > 0 ? board[rowIndex - 1][colIndex] : null;
+  const checkTileBelow =
     rowIndex < board.length - 1 ? board[rowIndex + 1][colIndex] : null;
-  const checkLeftTile = colIndex > 0 ? board[rowIndex][colIndex - 1] : null;
-  const checkRightTile =
+  const checkTileToLeft = colIndex > 0 ? board[rowIndex][colIndex - 1] : null;
+  const checkTileToRight =
     colIndex < board[rowIndex].length - 1
       ? board[rowIndex][colIndex + 1]
       : null;
 
   // If the tile above, under, left or right of the clicked tile is empty, return its row and column index
-  if (checkAboveTile === 0) {
+  if (checkTileAbove === 0) {
     return { emptyRow: rowIndex - 1, emptyCol: colIndex };
   }
-  if (checkBelowTile === 0) {
+  if (checkTileBelow === 0) {
     return { emptyRow: rowIndex + 1, emptyCol: colIndex };
   }
-  if (checkLeftTile === 0) {
+  if (checkTileToLeft === 0) {
     return { emptyRow: rowIndex, emptyCol: colIndex - 1 };
   }
-  if (checkRightTile === 0) {
+  if (checkTileToRight === 0) {
     return { emptyRow: rowIndex, emptyCol: colIndex + 1 };
   }
   return null;
