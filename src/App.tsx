0a3153262e019130ba724218841b7a47be231e47
Felix Odenhall <felix.odenhall@gmail.com>
2023-03-26 12:56:55 +0200

    Added SheadLine to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,22 @@ import styled from "styled-components";
 import { GameBoard } from "./components";
 import "./index.css";
 
-const SContainer = styled.div`
-  height: 90vh;
+const SContainer = styled.main`
+  height: 80vh;
 `;
+
+const SHeadLine1 = styled.h1`
+  font-size: 2rem;
+  font-weight: 700;
+  text-align: center;
+  font-size: 2rem;
+  margin-top: 50px;
+`;
+
 const App = () => {
   return (
     <SContainer>
+      <SHeadLine1>Sliding puzzle</SHeadLine1>
       <GameBoard />
     </SContainer>
   );
