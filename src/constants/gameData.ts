9b04ebec0360dd04f7d89628d05ddad9684a9abb
felix-odenhall <felix.odenhall@gmail.com>
2023-03-27 10:42:15 +0200

    updated initialRows & initialColumns to 3x5 instead of 10x10

--- src/constants/gameData.ts
+++ src/constants/gameData.ts
@@ -1,4 +1,4 @@
-export const initialRows = 10;
-export const initialColumns = 10;
+export const initialRows = 3;
+export const initialColumns = 5;
 export const tile_size =
   (initialRows * initialColumns) / 3 >= 5 ? "3.5rem" : "5rem";
